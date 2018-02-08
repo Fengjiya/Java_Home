ff35822265aee46603660b91c4d98842b1e3102b
Fengjiya <fengjiya@gmail.com>
2018-02-08 20:21:46 +0800

    提交内容：SpringBoot运行成功 提交人：fengjiya

---
+++ springboot-demo2/src/main/java/fengjiya/cai/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package fengjiya.cai.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("\n我就做个实验\n");
+	}
+}
